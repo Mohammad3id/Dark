a0f11fa6ba9811665c7e9716bd3710dc1a02df7a
Mohammad3id <eid115599@gmail.com>
2020-07-29 12:35:32 +0200

    fully implemented navbar button

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 nav_button = document.querySelector(".navbar-button");
-nav_items = document.querySelectorAll(".navbar-item");
 nav_items_box = document.querySelector(".navbar-items");
 
 nav_button.addEventListener("click", () => {
 	nav_items_box.classList.toggle("toggle");
+	nav_button.classList.toggle("toggle");
 });
